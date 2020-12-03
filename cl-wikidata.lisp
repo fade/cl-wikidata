c7a9899a8ec7f5d18e50f93a9d741dbb2e284796
Brian O'Reilly <fade@deepsky.com>
2020-12-03 15:53:39 -0500

    start of cleaning up the readme file, some test queries.

--- cl-wikidata.lisp
+++ cl-wikidata.lisp
@@ -1,12 +1,27 @@
 (defpackage :cl-wikidata
             (:use :cl)
             (:use :cl-wikidata.app-utils
-                  :dexador :alexandria :cl-json-pointer)
+                  :alexandria :cl-json-pointer)
             (:export :-main))
 
 (in-package :cl-wikidata)
 
-(defparameter *SPARQL* "https://query.wikidata.org/sparql?query=~A&format=json")
+(defparameter *SPARQL* "https://query.wikidata.org/sparql?query=\"~A\"&format=json")
+
+(defparameter *query*
+  "SELECT ?lemma ?item WHERE {
+  VALUES ?lemma {
+    \"Wikipedia\"@de
+    \"Wikidata\"@de
+    \"Berlin\"@de
+    \"Technische Universität Berlin\"@de
+  }
+  ?sitelink schema:about ?item;
+    schema:isPartOf <https://de.wikipedia.org/>;
+    schema:name ?lemma.
+}")
+
+
 
 (defun -main (&optional args)
   (format t "~a~%" "I don't do much yet"))
