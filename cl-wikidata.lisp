22f5fd6462bc1d147e0651c54b4bdc5e0921b88b
Brian O'Reilly <fade@deepsky.com>
2021-01-30 12:10:38 -0500

    Delete dead code.

--- cl-wikidata.lisp
+++ cl-wikidata.lisp
@@ -67,71 +67,6 @@ ORDER BY ?airportLabel"
                 (get-by-json-pointer obj (format nil "/results/bindings/~D/icaocode/value" i))
                 (get-by-json-pointer obj (format nil "/results/bindings/~D/iatacode/value" i))))))
 
-;; (defun getdata (&key (query *query*) (endpoint *wdataq*) (user-agent *user-agent*))
-;;   "Connect to :endpoint and ship the query."
-;;   (format t "~&!!~A||~A||~A" query endpoint user-agent)
-;;   (let* ((qri query)) ;; (quri:url-encode query) dex implicitly encodes query parameters.
-;;     (multiple-value-bind (body status headers uri connection)
-;;         (handler-case
-;;             (dex:request endpoint
-;;                          :method :get
-;;                          :headers '(("User-Agent" . "CL-WIKIDATA/0.0 (https://github.com/fade/cl-wikidata.git; fade@deepsky.com) sbcl/2.1.0")
-;;                                     ("accept" . "application/json"))
-;;                          :content '(("format" . "json")
-;;                                     ("query" . qri))
-;;                          :verbose t)
-;;           (error (c) c))
-;;       (setf *getreturn* body)
-;;       (format t "~&~{~A~^~%~}" (list status headers (quri:render-uri uri) connection))))
-;;   ;; body  
-;;   )
-
-
-
-;; (defun getdata-drakma (&key (query *query*) (endpoint *wdataq*) (user-agent *user-agent*))
-;;   "Connect to :endpoint with drakma and ship the query."
-;;   (multiple-value-bind (body status headers uri connection)
-;;       (handler-case
-;;           (drakma:http-request endpoint
-;;                                ;; :method :post
-;;                                :user-agent :explorer
-;;                                         :parameters '(("format" . "json")
-;;                                                       ("query" . query))))
-;;     (setf *getreturn* body)
-;;     (format t "~&~{~A~^~%~}" (list status headers uri connection))))
-
-;;;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-;;; test post function
-;;;=============================================================================
-;; (defun bonk (&key (query *query*) (endpoint *wdataq*) (user-agent *user-agent*))
-;;   (multiple-value-list ;; (body status headers uri connection)
-;;    (let* ((query query)
-;;           (endpoint endpoint)
-;;           (user-agent user-agent))
-;;     (handler-case
-;;         (dex:request endpoint
-;;                      :method :post
-;;                      :headers '(("User-Agent" . user-agent)
-;;                                 ("accept" . "application/sparql-results+json"))
-;;                      :content '(("query" . query) ("format" . "json")))
-;;       (error (c) c)))))
-;;;=============================================================================
-
-
-
-;; (defun postdata (&key (query *query*) (endpoint *wdataq*) (user-agent *user-agent*))
-;;   "send a wikidata query as a post action to the sparql endpoint."
-;;   (multiple-value-list ;; (body status headers uri connection)
-;;    (handler-case
-;;        (dex:request endpoint
-;;                     :method :post
-;;                     :headers '(("User-Agent" . *user-agent*)
-;;                                ("accept" . "application/sparql-results+json"))
-;;                     :content '(("query" . query) ("format" . "json")))
-;;      (error (c) c))
-;;    (setf *getreturn* body)
-;;    (format t "~&~{~A~^~%~}" (list status headers uri connection))))
-
 (defun write-json-to-file (json file)
   (with-open-file (s file :direction :output :if-exists :supersede)
     (write-sequence json s)))
@@ -140,8 +75,6 @@ ORDER BY ?airportLabel"
   (multiple-value-bind (body status headers uri connection)
       (dexador:get address)
     (list body status headers uri connection)))
-
-;; 😀
 
 (defun -main (&optional args)
   (declare (ignorable args))
